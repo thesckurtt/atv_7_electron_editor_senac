661996dabe127996af4af9005b9984434d2b6278
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-20 12:05:37 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'electron', // <- impede que o Electron seja empacotado
+        'fs',
+        'path',
+        'os'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  }
 })
