82c721ac11a7ab91af1ecbbccd753343c3fada99
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-05-20 09:18:58 -0300

    first commit

---
+++ electron/main.js
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
